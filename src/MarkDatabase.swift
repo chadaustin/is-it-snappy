1f25a87640e4754b90632e1c409841c5dc86553d
Chad Austin <chad@chadaustin.me>
2017-10-15 12:59:20 -0700

    emdash is nicer

--- src/MarkDatabase.swift
+++ src/MarkDatabase.swift
@@ -14,10 +14,10 @@ struct Mark {
         }
         
         switch (name ?? defaultName, elapsed) {
-        case let (.some(name), .some(elapsed)): return "\(name) -- \(elapsed) ms"
-        case let (.some(name), .none): return "\(name) --"
-        case let (.none, .some(elapsed)): return "-- \(elapsed) ms"
-        case (.none, .none): return "--"
+        case let (.some(name), .some(elapsed)): return "\(name) — \(elapsed) ms"
+        case let (.some(name), .none): return "\(name) —"
+        case let (.none, .some(elapsed)): return "— \(elapsed) ms"
+        case (.none, .none): return "—"
         }
     }
 
