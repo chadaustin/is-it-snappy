27f05989c49cca11a576e71a1c729b503f7178fb
Leonard Hecker <leonard@hecker.io>
2020-11-01 15:15:42 +0100

    Fix compilation for iOS 14

--- src/CaptureListViewController.swift
+++ src/CaptureListViewController.swift
@@ -129,7 +129,7 @@ class CaptureListViewController: UIViewController, UITableViewDataSource, UITabl
                 case .notDetermined, .restricted, .denied:
                     self?.captureButton.isEnabled = true
                     showSettingsAlert(permission: "Photos")
-                case .authorized:
+                default:
                     AVCaptureDevice.requestAccess(for: AVMediaType.video) { granted in
                         DispatchQueue.main.async {
                             self?.captureButton.isEnabled = true
