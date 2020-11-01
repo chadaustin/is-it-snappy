2080cdde524a161849998bdc41f97bf4f4a4bfb0
Leonard Hecker <leonard@hecker.io>
2020-11-01 15:18:40 +0100

    Fix frame time extraction for out-of-order video frames

--- src/MarkViewController.swift
+++ src/MarkViewController.swift
@@ -40,6 +40,9 @@ class PlayerInfo {
             }
         }
 
+        // H.264 and later can store video frames out-of-order for better compression.
+        // --> It's necessary to sort the frame times.
+        times.sort(by: <)
         frameTimes = times
     }
 
