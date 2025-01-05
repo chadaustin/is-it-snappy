f00119343cd078aaecfab9833919efb9f4a57d82
Chad Austin <chad@chadaustin.me>
2025-01-04 23:15:59 -0800

    fix the layout of the help page

--- src/HelpViewController.swift
+++ src/HelpViewController.swift
@@ -2,13 +2,9 @@ import UIKit
 import WebKit
 
 class HelpViewController: UIViewController {
-    let webView = WKWebView()
+    @IBOutlet var webView: WKWebView!
 
-    override func loadView() {
-        view = webView
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
+    override func viewDidLoad() {
         let path = Bundle.main.path(forResource: "help", ofType: "html")!
         let url = URL(fileURLWithPath: path)
         webView.loadFileURL(url, allowingReadAccessTo: url)
