1984a2b1c884df949c3593cf86c0a52fee9fbae0
Chad Austin <chad@chadaustin.me>
2025-01-04 23:06:10 -0800

    remove some Swift 4.2 migration junk

--- src/AboutViewController.swift
+++ src/AboutViewController.swift
@@ -10,19 +10,14 @@ class AboutViewController: UIViewController {
     }
 
     @IBAction func handleTapHomepage() {
-        UIApplication.shared.open(homepageURL, options: convertToUIApplicationOpenExternalURLOptionsKeyDictionary([:]), completionHandler: nil)
+        UIApplication.shared.open(homepageURL, options: [:], completionHandler: nil)
     }
 
     @IBAction func handleTapGitHub() {
-        UIApplication.shared.open(githubURL, options: convertToUIApplicationOpenExternalURLOptionsKeyDictionary([:]), completionHandler: nil)
+        UIApplication.shared.open(githubURL, options: [:], completionHandler: nil)
     }
 
     @IBAction func handleTapTwitter() {
-        UIApplication.shared.open(twitterURL, options: convertToUIApplicationOpenExternalURLOptionsKeyDictionary([:]), completionHandler: nil)
+        UIApplication.shared.open(twitterURL, options: [:], completionHandler: nil)
     }
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertToUIApplicationOpenExternalURLOptionsKeyDictionary(_ input: [String: Any]) -> [UIApplication.OpenExternalURLOptionsKey: Any] {
-	return Dictionary(uniqueKeysWithValues: input.map { key, value in (UIApplication.OpenExternalURLOptionsKey(rawValue: key), value)})
 }
