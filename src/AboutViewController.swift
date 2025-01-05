5990e40e6da755084da1bc49e7252ecb587215df
Chad Austin <chad@chadaustin.me>
2025-01-04 22:38:16 -0800

    add a homepage link to the about page

--- src/AboutViewController.swift
+++ src/AboutViewController.swift
@@ -1,11 +1,16 @@
 import UIKit
 
+let homepageURL: URL = URL(string: "https://isitsnappy.com")!
 let githubURL: URL = URL(string: "https://github.com/chadaustin/is-it-snappy")!
 let twitterURL: URL = URL(string: "https://twitter.com/chadaustin")!
 
 class AboutViewController: UIViewController {
     @IBAction func handleBack() {
         dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func handleTapHomepage() {
+        UIApplication.shared.open(homepageURL, options: convertToUIApplicationOpenExternalURLOptionsKeyDictionary([:]), completionHandler: nil)
     }
 
     @IBAction func handleTapGitHub() {
